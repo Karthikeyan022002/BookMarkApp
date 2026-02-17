be37240de99ff3e45dc7c7a4d1edf73c0a76a4d6
Karthikeyan <karthikeyanofficial022002@gmail.com>
2026-02-17 14:58:06 +0530

    fix redirect

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: "`${window.location.origin}/dashboard`,
+        redirectTo: "`${window.location.origin}/dashboard`"
       },
     })
   }
